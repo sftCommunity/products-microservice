cbf9ef1d5adbf1c119647c5460ecee95eb2b31dc
David Agámez <davidagamez04@gmail.com>
2024-11-14 18:04:41 -0500

    refactor: update main.ts to create a microservice for products

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port,
+    },
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(envs.port);
-  logger.log(`Server is running on port ${envs.port}`);
+
+  await app.listen();
+  logger.log(`Products microservice running on port ${envs.port}`);
 }
 
 bootstrap();
