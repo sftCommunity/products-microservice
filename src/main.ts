11fc8037774830126426b5b9de230d7889735c21
David Agámez <davidagamez04@gmail.com>
2024-11-14 16:16:13 -0500

    feat: add global validation pipe
    Add a global validation pipe to the application to enforce whitelisting and forbid non-whitelisted properties in incoming requests.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`Server is running on port ${process.env.PORT ?? 3000}`);
 }
