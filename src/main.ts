0e49ae7632734bd2de3957c59b311a4a7ebead01
David Agámez <davidagamez04@gmail.com>
2024-11-14 15:55:11 -0500

    chore: feat product module created

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`Server is running on port ${process.env.PORT ?? 3000}`);
 }
+
 bootstrap();
